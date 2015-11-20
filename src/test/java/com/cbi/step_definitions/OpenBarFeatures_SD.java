e83ef979f5367b07275ee2e3fe6a6569671e59ac
pandeyh <himanshu.pandey12@gmail.com>
2015-11-20 12:31:13 -0800

    Modified Some Features

--- src/test/java/com/cbi/step_definitions/OpenBarFeatures_SD.java
+++ src/test/java/com/cbi/step_definitions/OpenBarFeatures_SD.java
@@ -54,24 +54,6 @@ public class OpenBarFeatures_SD implements En {
 			// assertEquals(driver.getTitle(),"TO DO" );
 		});
 
-		
-		When("^I typed \"([^\"]*)\" into search box \"([^\"]*)\"$", (String arg1, String arg2) -> {
-			driver = WebDriverConfig.getIE();
-			driver.findElement(By.xpath(arg2)).sendKeys(arg1);
-		});
-
-		When("^I click the search button  \"([^\"]*)\"$", (String arg1) -> {
-		    driver.findElement(By.className(arg1)).click();
-		});
-		
-		When("^I wait for search results (\\d+) seconds$", (Integer arg1) -> {
-			driver.manage().timeouts().implicitlyWait(arg1, TimeUnit.SECONDS);
-		});
-
-		Then("^I should get the search results \"([^\"]*)\"$", (String arg1) -> {
-			assertEquals(driver.findElement(By.xpath(arg1)).getText(),"TERMS OF USE" ); 
-		});
-
 		Then("^I close the browser$", () -> {
 			driver.quit();
 		});
